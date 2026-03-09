7492915b0b26d0e15efcff8c79f331b06f93a0fc
Lucas Caldardo Pereira <llucascalldardo@gmail.com>
2026-03-09 13:20:29 -0300

    feat: Cria pastas iniciais da arquitetura do projeto

---
+++ src/main/java/dev/lucascaldardo/desafio/itau/junior2024/controller/TransacoesController.java
@@ -0,0 +1,7 @@
+package dev.lucascaldardo.desafio.itau.junior2024.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransacoesController {
+}
---
+++ src/main/java/dev/lucascaldardo/desafio/itau/junior2024/model/TransacoesModel.java
@@ -0,0 +1,18 @@
+package dev.lucascaldardo.desafio.itau.junior2024.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransacoesModel {
+
+    private BigDecimal valor;
+    private OffsetDateTime dataHora;
+
+}
